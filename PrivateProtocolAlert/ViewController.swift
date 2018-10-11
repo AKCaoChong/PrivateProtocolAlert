62be1f0eb474586820a90dce3527ad0ea44875c5
caochong <caochong@xiaohe.com>
2018-10-11 16:41:29 +0800

    Initial Commit

---
+++ PrivateProtocolAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrivateProtocolAlert
+//
+//  Created by 曹冲 on 2018/10/11.
+//  Copyright © 2018年 曹冲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
